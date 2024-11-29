808871b2518fb5096c37a456d89f63139e269b3b
dokyung-kang <kangdk02@naver.com>
2024-11-29 12:18:45 +0900

    feat : 회원 도메인 실행과 테스트
    #1

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 구현체까지 의존하는 문제 있음(DIP 위반)
 
     @Override
     public void join(Member member) {
