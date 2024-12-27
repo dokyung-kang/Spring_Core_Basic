c5c0d7ff7cfe8b79bb9c34d1c20836127a5b55db
dokyung-kang <kangdk02@naver.com>
2024-12-27 15:32:56 +0900

    feat: 관심사의 분리
    #3

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 구현체까지 의존하는 문제 있음(DIP 위반)
+//    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 구현체까지 의존하는 문제 있음(DIP 위반)
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {   // 생성자 주입
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
